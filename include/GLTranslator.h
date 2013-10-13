76cae3e9169de2b16dc1f99778f7857e72602d35
sharpen70 <sharpen70@sina.cn>
2013-10-13 22:29:14 +0800

    add GL transformation

---
+++ include/GLTranslator.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   GLTranslator.h
+ * Author: sharpen
+ *
+ * Created on October 13, 2013, 10:28 PM
+ */
+
+#ifndef GLTRANSLATOR_H
+#define	GLTRANSLATOR_H
+
+class GLTranslator {
+public:
+    GLTranslator();
+    GLTranslator(const GLTranslator& orig);
+    virtual ~GLTranslator();
+private:
+
+};
+
+#endif	/* GLTRANSLATOR_H */
+
---
+++ src/GLTranslator.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   GLTranslator.cpp
+ * Author: sharpen
+ * 
+ * Created on October 13, 2013, 10:28 PM
+ */
+
+#include "GLTranslator.h"
+
+GLTranslator::GLTranslator() {
+}
+
+GLTranslator::GLTranslator(const GLTranslator& orig) {
+}
+
+GLTranslator::~GLTranslator() {
+}
+
